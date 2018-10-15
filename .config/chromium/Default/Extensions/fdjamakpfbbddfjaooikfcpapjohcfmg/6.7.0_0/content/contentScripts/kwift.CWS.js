a9d9a1ac622b69b288b845d1ef2f2e09e525fe08
lordie <levimanga@gmail.com>
2018-10-15 15:17:15 -0300

    bspwmrc outter gaps

--- .config/chromium/Default/Extensions/fdjamakpfbbddfjaooikfcpapjohcfmg/6.7.0_0/content/contentScripts/kwift.CWS.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var comPort = chrome.extension.connect({
-        name : "fromInjectedTo"
-    });
-
-    document.getElementById("uninstall-extension").addEventListener('click', function(event) {
-        comPort.postMessage("action=uninstallExtension");
-    });
-})();
